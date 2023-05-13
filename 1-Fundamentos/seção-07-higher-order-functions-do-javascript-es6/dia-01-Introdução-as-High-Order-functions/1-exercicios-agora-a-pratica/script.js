05b5547d1e16f021e5cf8882950a09318af52d65
Thiago Carpinetti <thiac12@gmail.com>
2023-05-12 22:38:11 -0300

    dia 02 e 03

---
+++ 1-Fundamentos/seção-07-higher-order-functions-do-javascript-es6/dia-01-Introdução-as-High-Order-functions/1-exercicios-agora-a-pratica/script.js
@@ -0,0 +1,148 @@
+// Exercicio 1
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().replace(' ', '_');
+  return { fullName, email: `${email}@trybe.com` };
+};
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
+  };
+  return employees;
+};
+
+// console.log(newEmployees(employeeGenerator));
+
+// Exercicio 2
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) +1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try again!';
+};
+
+// console.log(lotteryResult(2, numberChecker));
+
+// Exercicio 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnwers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {return 1;}
+  if (studentAnswer === 'N.A') {return 0;}
+  return -0.5;
+}
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+// console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnwers));
+
+// Exercicio 4
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const authorBornIn1947 = () => {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+};
+
+const smallerName = () => {
+  let nameBook;
+
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
+  return nameBook;
+};
+
+const getNamedBook = () => {
+  return books.find((book) => book.name.length === 26);
+};
+
+const everyoneWasBornOnSecXX = () => {
+  return books.every((book) => (
+    book.author.birthYear >= 1901 && book.author.birthYear <= 2000
+  ));
+};
+
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+};
+
+const authorUnique = () => {
+  return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
+};
